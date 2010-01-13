991ad8b3772b18f01b30db4ec7259f89ebc26d20
LuckyGeck <pasha.sychev@gmail.com>
2010-01-13 19:15:00 +0200

    v0.01

---
+++ PSint/frMain.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace PSint
+{
+    partial class frMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.stepIntoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stepOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dialOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dialSave = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProjectToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.runToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(604, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProjectToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem1,
+            this.stepIntoToolStripMenuItem,
+            this.stepOutToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.resetToolStripMenuItem});
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.runToolStripMenuItem.Text = "Run";
+            // 
+            // runToolStripMenuItem1
+            // 
+            this.runToolStripMenuItem1.Name = "runToolStripMenuItem1";
+            this.runToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.runToolStripMenuItem1.Text = "Run";
+            this.runToolStripMenuItem1.Click += new System.EventHandler(this.runToolStripMenuItem1_Click);
+            // 
+            // stepIntoToolStripMenuItem
+            // 
+            this.stepIntoToolStripMenuItem.Name = "stepIntoToolStripMenuItem";
+            this.stepIntoToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.stepIntoToolStripMenuItem.Text = "Step into";
+            // 
+            // stepOutToolStripMenuItem
+            // 
+            this.stepOutToolStripMenuItem.Name = "stepOutToolStripMenuItem";
+            this.stepOutToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.stepOutToolStripMenuItem.Text = "Step out";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(125, 6);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // dialOpen
+            // 
+            this.dialOpen.Filter = "PSint project (*.ps)|*.ps";
+            this.dialOpen.FileOk += new System.ComponentModel.CancelEventHandler(this.dialOpen_FileOk);
+            // 
+            // dialSave
+            // 
+            this.dialSave.DefaultExt = "ps";
+            this.dialSave.Filter = "PSint project (*.ps)|*.ps";
+            this.dialSave.FileOk += new System.ComponentModel.CancelEventHandler(this.dialSave_FileOk);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 381);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(604, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(604, 357);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox1_PreviewKeyDown);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // newProjectToolStripMenuItem
+            // 
+            this.newProjectToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProjectToolStripMenuItem1});
+            this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newProjectToolStripMenuItem.Text = "Create";
+            // 
+            // newProjectToolStripMenuItem1
+            // 
+            this.newProjectToolStripMenuItem1.Name = "newProjectToolStripMenuItem1";
+            this.newProjectToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.newProjectToolStripMenuItem1.Text = "New Project";
+            this.newProjectToolStripMenuItem1.Click += new System.EventHandler(this.newProjectToolStripMenuItem1_Click);
+            // 
+            // frMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 403);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PS interpretator - [Unknown]";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem stepIntoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stepOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog dialOpen;
+        private System.Windows.Forms.SaveFileDialog dialSave;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem1;
+    }
+}
+
---
+++ PSint/frMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PSint
+{
+    public partial class frMain : Form
+    {
+        string path = "";
+        
+        bool bTextChanged = false;
+        
+        public frMain()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool bWorkOn = true;
+            
+            if (bTextChanged)
+            {
+                switch (MessageBox.Show("This source file was not saved!\n Do you want to save it?", "Unsaved Changes", MessageBoxButtons.YesNoCancel))
+                {
+                    case DialogResult.Yes:
+                        saveAsToolStripMenuItem_Click(null, EventArgs.Empty);
+                        break;
+                    case DialogResult.No:
+                        break;
+                    case DialogResult.Cancel:
+                        bWorkOn = false;
+                        break;
+                }
+            }
+
+            if (bWorkOn) Application.Exit();
+        }
+
+        private void dialOpen_FileOk(object sender, CancelEventArgs e)
+        {
+            LoadFile(dialOpen.FileName);
+            bTextChanged = false;
+        }
+
+        private void LoadFile(string p)
+        {
+            textBox1.Text = File.ReadAllText(p, Encoding.UTF8);
+            path = p;
+            SetCaption();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool bWorkOn = true;
+            
+            if (bTextChanged)
+            {
+                switch (MessageBox.Show("This source file was not saved!\n Do you want to save it?", "Unsaved Changes", MessageBoxButtons.YesNoCancel))
+                {
+                    case DialogResult.Yes:
+                        saveAsToolStripMenuItem_Click(null, EventArgs.Empty);
+                        break;
+                    case DialogResult.No:
+                        break;
+                    case DialogResult.Cancel:
+                        bWorkOn = false;
+                        break;
+                }
+            }
+            
+            if (bWorkOn)
+            {
+                dialOpen.ShowDialog();
+                bTextChanged = false;
+            }
+        }
+
+        private void dialSave_FileOk(object sender, CancelEventArgs e)
+        {
+            SaveFile(dialSave.FileName);
+        }
+
+        private void SaveFile(string p)
+        {
+
+            FileStream FS = new FileStream(p, FileMode.OpenOrCreate);
+
+            StreamWriter SW = new StreamWriter(FS);
+            SW.Write(textBox1.Text);
+            SW.Close();
+            
+            FS.Close();
+
+            path = p;
+
+            SetCaption();
+        }
+
+        private void SetCaption()
+        {
+            if (path!="")
+            {
+
+                this.Text = "PS interpretator - [" + Path.GetFileName(path) + "]";
+                            }
+            else
+            {
+                this.Text = "PS interpretator - [Unknown]";
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dialSave.ShowDialog();
+            bTextChanged = false;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (path != "")
+            {
+                SaveFile(path);
+            }
+            else
+                dialSave.ShowDialog();
+            bTextChanged = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            bTextChanged = true;
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Control)
+            {
+                switch (e.KeyValue)
+                {
+                    case 'A': textBox1.SelectAll(); break;
+                    case 'C': textBox1.Copy(); break;
+                    case 'V': textBox1.Paste(); break;
+                }
+            }
+        }
+
+        private void newProjectToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            bool bWorkOn = true;
+            
+            if (bTextChanged)
+            {
+                switch (MessageBox.Show("This source file was not saved!\n Do you want to save it?", "Unsaved Changes", MessageBoxButtons.YesNoCancel))
+                {
+                    case DialogResult.Yes:
+                        saveAsToolStripMenuItem_Click(null, EventArgs.Empty);
+                        break;
+                    case DialogResult.No:
+                        break;
+                    case DialogResult.Cancel:
+                        bWorkOn = false;
+                        break;
+                }
+            }
+            if (bWorkOn)
+            {
+                path = "";
+                bTextChanged = false;
+                textBox1.Text = "";
+                SetCaption();
+            }
+            
+        }
+
+        private void runToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("IT'S RUNNING!");
+        }
+    }
+}
